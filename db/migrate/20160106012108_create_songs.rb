3d6130422196be1a654e85077cf8aceb0ee7f2b8
Tim Brown <brown.tim.lee@gmail.com>
2016-01-05 19:26:25 -0600

    added song model

--- db/migrate/20160106012108_create_songs.rb
+++ db/migrate/20160106012108_create_songs.rb
@@ -1,10 +1,12 @@
-class CreateSongsTable < ActiveRecord::Migration
+class CreateSongs < ActiveRecord::Migration
   def change
-    create_table :songs_tables do |t|
+    create_table :songs do |t|
       t.text :title
       t.text :artist
       t.integer :year
       t.integer :position
+
+      t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106011021) do
+ActiveRecord::Schema.define(version: 20160106012108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "songs_tables", force: :cascade do |t|
-    t.text    "title"
-    t.text    "artist"
-    t.integer "year"
-    t.integer "position"
+  create_table "songs", force: :cascade do |t|
+    t.text     "title"
+    t.text     "artist"
+    t.integer  "year"
+    t.integer  "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
